ee80e96132d814b1094888671a3754928bd8eda3
Christian Frauenstein <cfrauenstein@gmail.com>
2016-03-29 09:02:40 +0200

    New wall

--- js/main.js
+++ js/main.js
@@ -1,127 +1,145 @@
-	var camera, scene, renderer;
-			var mesh;
-			
-			var clock = new THREE.Clock();
-			
-			
-			init();
-			animate();
-			
-			
-			
-			function createNewPainting(src, width, height, x, y ,z){
-				
-				thickness = 5;
-				painting = new THREE.Object3D();
-								
-				var grootvlakG = new THREE.PlaneGeometry( width, height );
-				var kleinvlakG = new THREE.PlaneGeometry( thickness, height );
-				
-				
-				var artwork = THREE.ImageUtils.loadTexture( src );
-				artwork.anisotropy = renderer.getMaxAnisotropy();
-				
-				
-				grootvlakM = new THREE.MeshBasicMaterial( { map: artwork, side: THREE.DoubleSide } );
-				kleinvlakM = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
-				
-				mainFrontMesh = new THREE.Mesh( grootvlakG, grootvlakM );
-				mainFrontMesh.position = new THREE.Vector3( x, y, z );
-				mainBackMesh = new THREE.Mesh( grootvlakG, grootvlakM );
-				mainBackMesh.position = new THREE.Vector3( x, y, z-thickness );
-				
-				leftMesh = new THREE.Mesh(kleinvlakG,kleinvlakM);
-				leftMesh.rotation = new THREE.Euler( 0, Math.PI/2, 0, 'XYZ' );
-				leftMesh.position = new THREE.Vector3( (width/2)+x, y, z+(-thickness/2) );
-				
-				rightMesh = new THREE.Mesh(kleinvlakG,kleinvlakM);
-				rightMesh.rotation = new THREE.Euler( 0, Math.PI/2, 0, 'XYZ' );
-				rightMesh.position = new THREE.Vector3( (-width/2)+x, y, z+(-thickness/2) );
-				
-				
-						
-				painting.add( mainFrontMesh );
-				painting.add( mainBackMesh );
-				painting.add( leftMesh );
-				painting.add( rightMesh );
-				
-				return painting;
-			
-			}
-			
-			
-			function init() {
+var camera, scene, renderer;
+var mesh;
 
-				renderer = new THREE.WebGLRenderer();
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				document.body.appendChild( renderer.domElement );
+var clock = new THREE.Clock();
 
-				//
+init();
+animate();
 
-				camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-				camera.position.z = 100;
-				
-				controls = new THREE.FirstPersonControls( camera );
+function createNewPainting(src, width, height, _thickness){
 
-				controls.movementSpeed = 200;
-				controls.lookSpeed = 0.125;
-				controls.lookVertical = true;
+    thickness = _thickness;
+    painting = new THREE.Object3D();
 
-				scene = new THREE.Scene();
+    var painting_geometry = new THREE.PlaneGeometry( width, height );
+    var side_geomerty = new THREE.PlaneGeometry( thickness, height );
+    var topbottom_geomerty = new THREE.PlaneGeometry( thickness, width );
 
-				/*var geometry = new THREE.BoxGeometry( 10, 200, 200 );
+    var artwork = THREE.ImageUtils.loadTexture( src );
+    artwork.anisotropy = renderer.getMaxAnisotropy();
 
-				var texture = THREE.ImageUtils.loadTexture( 'img/beach2.jpg' );
-				texture.anisotropy = renderer.getMaxAnisotropy();
+    var painting_material = new THREE.MeshBasicMaterial( { map: artwork, side: THREE.DoubleSide } );
+    var side_material = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
 
-				var material = new THREE.MeshBasicMaterial( { map: texture } );
+    var painting_mesh = new THREE.Mesh( painting_geometry, painting_material );
+    painting_mesh.position = new THREE.Vector3( 0, 0, 0 + thickness);
 
-				mesh = new THREE.Mesh( geometry, material );
-				scene.add( mesh );*/
-				
-				p1 = createNewPainting('img/beach1.jpg', 100, 100, 0,0,0);
-				
-				p2 = createNewPainting('img/beach2.jpg', 100, 100, 200,0,0);
-				
-				wall = createNewPainting('img/wall.jpg', 1000, 400, 0,0,-5);
-				
-				
-				
-				
-				scene.add(p1);	
-				scene.add(p2);	
-				scene.add(wall);	
-				
-				/*
-				
-				var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-				plane = new THREE.Mesh( geometry, material );
-				scene.add( plane )*/
+    var left_side_mesh = new THREE.Mesh(side_geomerty,side_material);
+    left_side_mesh.rotation = new THREE.Euler( 0, Math.PI/2, 0, 'XYZ' );
+    left_side_mesh.position = new THREE.Vector3( (width/2), 0, (thickness*0.5) );
 
-				//
+    var right_side_mesh = new THREE.Mesh(side_geomerty,side_material);
+    right_side_mesh.rotation = new THREE.Euler( 0, Math.PI/2, 0, 'XYZ' );
+    right_side_mesh.position = new THREE.Vector3( (-width/2), 0, (thickness*0.5) );
 
-				window.addEventListener( 'resize', onWindowResize, false );
+    var bottom_side_mesh = new THREE.Mesh(topbottom_geomerty,side_material);
+    bottom_side_mesh.rotation = new THREE.Euler( Math.PI/2, 0, Math.PI/2, 'XYZ' );
+    bottom_side_mesh.position = new THREE.Vector3( 0, (height/2), (thickness*0.5) );
 
-			}
+    var top_side_mesh = new THREE.Mesh(topbottom_geomerty,side_material);
+    top_side_mesh.rotation = new THREE.Euler( Math.PI/2, 0, Math.PI/2, 'XYZ' );
+    top_side_mesh.position = new THREE.Vector3( 0, (-height/2), (thickness*0.5) );
 
-			function onWindowResize() {
+    painting.add( painting_mesh );
+    painting.add( left_side_mesh );
+    painting.add( right_side_mesh );
+    painting.add( bottom_side_mesh );
+    painting.add( top_side_mesh );
 
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
+    // var painting_container = new THREE.Object3D();
+    // painting_container.add(painting);
+    return painting;
 
-				renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
-				controls.handleResize();
+function createNewWall(wallTexture, width, height){
 
-			}
+    var wall = new THREE.Object3D();
+    var wall_geometry = new THREE.PlaneGeometry( width, height );
+    var wall_material = new THREE.MeshBasicMaterial( { map: wallTexture, side: THREE.DoubleSide } );
+    wallTexture.anisotropy = renderer.getMaxAnisotropy();
 
-			function animate() {
+    var wall_mesh = new THREE.Mesh( wall_geometry, wall_material );
+    wall_mesh.position = new THREE.Vector3( 0, 0, 0 );
 
-				requestAnimationFrame( animate );
-				controls.update( clock.getDelta() );
-				//plane.rotation.x += 0.005;
-				//p1.rotation.y += 0.01;
+    wall.add(wall_mesh);
+    wall.rotation.set(0,0,0, 'XYZ');
 
-				renderer.render( scene, camera );
+    return wall;
 
-			}
+}
+
+
+function init() {
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera.position.x = -500;
+
+
+    controls = new THREE.FirstPersonControls( camera );
+    controls.verticalMin = 1.0;
+    controls.verticalMax = 2.0;
+    controls.noFly = true;
+    controls.movementSpeed = 400;
+    controls.lookSpeed = 0.2;
+    controls.lookVertical = false;
+
+    scene = new THREE.Scene();
+
+    //3072 x 2304 img/wall.jpg
+    var wall_texture = THREE.ImageUtils.loadTexture( 'img/wall.jpg' );
+
+
+    var ru = createNewPainting('img/ru.jpg', 40, 40, 3);
+    ru.position = new THREE.Vector3(0, 50, 0);
+    var jen = createNewPainting('img/jen.jpg', 40, 40, 3);
+    jen.position = new THREE.Vector3(200, 50, 0);
+    wall = createNewWall(wall_texture, 610, 200);
+    wall.rotation.set(0,-Math.PI/2,0, 'XYZ');
+    wall.add(ru);
+    wall.add(jen);
+
+    scene.add(wall);
+
+    var smaati1 = createNewPainting('img/smaati1.jpg', 1 * 50, (249/236) * 50, 7);
+    smaati1.position = new THREE.Vector3(0, 50, 0);
+    var smaati2 = createNewPainting('img/smaati2.jpg', 1 * 50, (196/236) * 50, 7);
+    smaati2.position = new THREE.Vector3(-100, 50, 0);
+    var wall2 = createNewWall(wall_texture, 610, 200);
+    wall2.rotation.set(0,0,0, 'XYZ');
+    wall2.position = new THREE.Vector3(-300, 0, -300);
+    wall2.add(smaati1);
+    wall2.add(smaati2);
+
+    scene.add(wall2);
+
+
+
+    window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    controls.handleResize();
+
+}
+
+function animate() {
+
+    requestAnimationFrame( animate );
+    controls.update( clock.getDelta() );
+    //wall.rotation.set(0,clock.getElapsedTime(), 0);
+    //camera.rotation.set(0,clock.getElapsedTime(), 0, 'XYZ');
+
+    //console.log(clock.getElapsedTime());
+    renderer.render( scene, camera );
+
+}
